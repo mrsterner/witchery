6914aacbebad6cc507655665f8abf0a2f1b91667
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-10-13 10:41:20 -0500

    Added Tag Handling and Adding all old Witchery values, still in progress

--- common/src/main/kotlin/dev/sterner/witchery/data/NaturePowerHandler.kt
+++ common/src/main/kotlin/dev/sterner/witchery/data/NaturePowerHandler.kt
@@ -6,30 +6,47 @@ import com.mojang.logging.LogUtils
 import com.mojang.serialization.Codec
 import com.mojang.serialization.JsonOps
 import com.mojang.serialization.codecs.RecordCodecBuilder
+import dev.architectury.event.events.common.LifecycleEvent
 import dev.architectury.registry.ReloadListenerRegistry
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.packs.PackType
 import net.minecraft.server.packs.resources.PreparableReloadListener
 import net.minecraft.server.packs.resources.ResourceManager
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener
+import net.minecraft.tags.TagKey
 import net.minecraft.util.profiling.ProfilerFiller
+import net.minecraft.world.SimpleContainer
+import net.minecraft.world.item.crafting.RecipeType
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.LevelEvent
+import net.minecraft.world.level.block.entity.SmokerBlockEntity
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.Executor
-import java.util.logging.Logger
 
 object NaturePowerHandler {
     private val LOGGER = LogUtils.getLogger()
     private val LOADER = NaturePowerLoader()
-
     private val NATURE_POWER_VALUES = mutableMapOf<ResourceLocation, Pair<Int, Int>>()
 
-    private val CODEC = RecordCodecBuilder.create { instance -> instance.group(
+    private val BLOCK_CODEC = RecordCodecBuilder.create { instance -> instance.group(
         ResourceLocation.CODEC.fieldOf("block").forGetter(Triple<ResourceLocation, Int, Int>::first),
         Codec.INT.fieldOf("power").forGetter(Triple<ResourceLocation, Int, Int>::second),
         Codec.INT.fieldOf("limit").forGetter(Triple<ResourceLocation, Int, Int>::third)
     ).apply(instance, ::Triple) }
+
+    private val TAG_CODEC = RecordCodecBuilder.create { instance -> instance.group(
+        ResourceLocation.CODEC.fieldOf("tag").forGetter(Triple<ResourceLocation, Int, Int>::first),
+        Codec.INT.fieldOf("power").forGetter(Triple<ResourceLocation, Int, Int>::second),
+        Codec.INT.fieldOf("limit").forGetter(Triple<ResourceLocation, Int, Int>::third)
+    ).apply(instance, ::Triple) }
+
+    /**
+     * Do not touch. This is simply used to handle tags AFTER they are loaded!
+     */
+    val tagQueue = ConcurrentLinkedQueue<Triple<ResourceLocation, Int, Int>>()
 
     /**
      * This method gets the base power provided by the given Block.
@@ -41,6 +58,34 @@ object NaturePowerHandler {
      * Please check against this value to determine if its base power should be added!!!!
      */
     fun getLimit(block: Block): Int? = NATURE_POWER_VALUES[BuiltInRegistries.BLOCK.getKey(block)]?.second
+
+    private fun addBlock(block: Block, power: Int, limit: Int) {
+        val loc = BuiltInRegistries.BLOCK.getKey(block)
+
+        if (NATURE_POWER_VALUES.containsKey(loc))
+            LOGGER.info("Overriding $loc from power ${NATURE_POWER_VALUES[loc]?.first} and limit ${NATURE_POWER_VALUES[loc]?.second} with a power of $power and a limit of $limit")
+        else
+            LOGGER.info("Registering $loc with a base power of $power and a limit of $limit")
+
+        NATURE_POWER_VALUES[loc] = Pair(power, limit)
+    }
+
+    private fun addTag(tag: TagKey<Block>, power: Int, limit: Int) {
+        val blocks = BuiltInRegistries.BLOCK.getTag(tag)
+        if (blocks.isPresent)
+            blocks.get().forEach { addBlock(it.value(), power, limit) }
+        else
+            LOGGER.error("Invalid Tag ${tag.location}!!!! Skipping it...")
+    }
+
+    /**
+     * Only call after world (read datapacks) are loaded so we can handle tags.
+     */
+    fun addPendingTags() {
+        tagQueue.forEach { (loc, power, limit) ->
+            addTag(TagKey.create(Registries.BLOCK, loc), power, limit)
+        }
+    }
 
     /**
      * This function is for registering the reload listener.
@@ -69,20 +114,25 @@ object NaturePowerHandler {
             resourceManager: ResourceManager,
             profiler: ProfilerFiller
         ) {
+            LifecycleEvent.SERVER_LEVEL_LOAD.register { level ->
+                addPendingTags()
+            }
+
             `object`.forEach { (file, element) ->
-                // Handle JSON here
                 try {
                     element.asJsonArray.map(JsonElement::getAsJsonObject).forEach { json ->
-                        val (loc, power, limit) = CODEC.decode(JsonOps.INSTANCE, json).getOrThrow(::IllegalArgumentException).first
+                        if (json.has("tag")) {
+                            tagQueue.add(TAG_CODEC.decode(JsonOps.INSTANCE, json).getOrThrow(::IllegalArgumentException).first)
+                        } else if (json.has("block")) {
+                            val (loc, power, limit) = BLOCK_CODEC.decode(JsonOps.INSTANCE, json)
+                                .getOrThrow(::IllegalArgumentException).first
 
-                        if (BuiltInRegistries.BLOCK.getOptional(loc).isPresent) {
-                            if (NATURE_POWER_VALUES.containsKey(loc))
-                                LOGGER.info("Overriding $loc from power ${NATURE_POWER_VALUES[loc]?.first} and limit ${NATURE_POWER_VALUES[loc]?.second} with a power of $power and a limit of $limit")
-                            else
-                                LOGGER.info("Registering $loc with a base power of $power and a limit of $limit")
-                            NATURE_POWER_VALUES[loc] = Pair(power, limit)
+                            val block = BuiltInRegistries.BLOCK.getOptional(loc)
+
+                            if (block.isPresent) addBlock(block.get(), power, limit)
+                            else LOGGER.error("Invalid Block $loc from file $file!!!! Skipping it...")
                         } else
-                            LOGGER.error("Invalid Block $loc from file $file!!!! Skipping it...")
+                            LOGGER.error("JSON missing block or tag in file $file!!!! Skipping it...")
                     }
                 } catch (e: Exception) {
                     throw IllegalArgumentException(e.fillInStackTrace())
