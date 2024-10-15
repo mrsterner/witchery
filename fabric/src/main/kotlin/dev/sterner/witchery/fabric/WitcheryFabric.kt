bbab35d5acc35e0a7bedeb3c9a30c2c8514253d5
mrsterner <simon@sterner.dev>
2024-10-15 20:22:15 +0200

    fix attachment deserialize

--- fabric/src/main/kotlin/dev/sterner/witchery/fabric/WitcheryFabric.kt
+++ fabric/src/main/kotlin/dev/sterner/witchery/fabric/WitcheryFabric.kt
@@ -1,11 +1,15 @@
 package dev.sterner.witchery.fabric
 
 import dev.sterner.witchery.Witchery
+import dev.sterner.witchery.api.attachment.MutandisLevelAttachment
 import dev.sterner.witchery.client.particle.ColorBubbleParticle
+import dev.sterner.witchery.platform.MutandisLevelDataAttachmentPlatform
 import dev.sterner.witchery.registry.WitcheryBlocks
 import dev.sterner.witchery.registry.WitcheryParticleTypes
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry
+import net.fabricmc.fabric.api.attachment.v1.AttachmentType
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.fabricmc.fabric.api.client.particle.v1.FabricSpriteProvider
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry
@@ -13,6 +17,15 @@ import net.minecraft.client.renderer.RenderType
 
 
 class WitcheryFabric : ModInitializer, ClientModInitializer {
+
+    companion object {
+        @Suppress("UnstableApiUsage")
+        val LEVEL_DATA_TYPE: AttachmentType<MutandisLevelAttachment> =
+            AttachmentRegistry.builder<MutandisLevelAttachment>()
+                .persistent(MutandisLevelDataAttachmentPlatform.CODEC)
+                .initializer { MutandisLevelAttachment() }
+                .buildAndRegister(MutandisLevelDataAttachmentPlatform.ID)
+    }
 
     override fun onInitialize() {
         Witchery.init()
--- fabric/src/main/kotlin/dev/sterner/witchery/platform/fabric/MutandisLevelDataAttachmentPlatformImpl.kt
+++ fabric/src/main/kotlin/dev/sterner/witchery/platform/fabric/MutandisLevelDataAttachmentPlatformImpl.kt
@@ -2,6 +2,7 @@ package dev.sterner.witchery.platform.fabric
 
 import dev.sterner.witchery.api.attachment.MutandisData
 import dev.sterner.witchery.api.attachment.MutandisLevelAttachment
+import dev.sterner.witchery.fabric.WitcheryFabric
 import dev.sterner.witchery.platform.MutandisLevelDataAttachmentPlatform
 import dev.sterner.witchery.platform.MutandisLevelDataAttachmentPlatform.CACHE_LIFETIME
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry
@@ -16,51 +17,45 @@ object MutandisLevelDataAttachmentPlatformImpl {
     @JvmStatic
     @Suppress("UnstableApiUsage")
     fun getMap(level: ServerLevel): MutableMap<BlockPos, MutandisData> {
-        return  level.getAttachedOrCreate(LEVEL_DATA_TYPE).mutandisCacheMap
+        return  level.getAttachedOrCreate(WitcheryFabric.LEVEL_DATA_TYPE).mutandisCacheMap
     }
 
     @JvmStatic
     @Suppress("UnstableApiUsage")
     fun getTagForBlockPos(level: ServerLevel, pos: BlockPos): TagKey<Block>? {
-        val attachments = level.getAttachedOrCreate(LEVEL_DATA_TYPE)
+        val attachments = level.getAttachedOrCreate(WitcheryFabric.LEVEL_DATA_TYPE)
         return attachments.mutandisCacheMap[pos]?.tag
     }
 
     @JvmStatic
     @Suppress("UnstableApiUsage")
     fun setTagForBlockPos(level: ServerLevel, pos: BlockPos, tag: TagKey<Block>)  {
-        level.getAttachedOrCreate(LEVEL_DATA_TYPE).mutandisCacheMap[pos] = MutandisData(tag, CACHE_LIFETIME)
+        level.getAttachedOrCreate(WitcheryFabric.LEVEL_DATA_TYPE).mutandisCacheMap[pos] = MutandisData(tag, CACHE_LIFETIME)
     }
 
     @JvmStatic
     @Suppress("UnstableApiUsage")
     fun removeTagForBlockPos(level: ServerLevel, pos: BlockPos)  {
-        level.getAttachedOrCreate(LEVEL_DATA_TYPE).mutandisCacheMap.remove(pos)
+        level.getAttachedOrCreate(WitcheryFabric.LEVEL_DATA_TYPE).mutandisCacheMap.remove(pos)
     }
 
     @JvmStatic
     @Suppress("UnstableApiUsage")
     fun updateTimeForTagBlockPos(level: ServerLevel, pos: BlockPos)  {
-        val data = level.getAttachedOrCreate(LEVEL_DATA_TYPE).mutandisCacheMap[pos]
+        val data = level.getAttachedOrCreate(WitcheryFabric.LEVEL_DATA_TYPE).mutandisCacheMap[pos]
         if (data != null) {
-            level.getAttachedOrCreate(LEVEL_DATA_TYPE).mutandisCacheMap[pos] = MutandisData(data.tag, data.time - 1)
+            level.getAttachedOrCreate(WitcheryFabric.LEVEL_DATA_TYPE).mutandisCacheMap[pos] = MutandisData(data.tag, data.time - 1)
         }
     }
 
     @JvmStatic
     @Suppress("UnstableApiUsage")
     fun resetTimeForTagBlockPos(level: ServerLevel, pos: BlockPos) {
-        val data = level.getAttachedOrCreate(LEVEL_DATA_TYPE).mutandisCacheMap[pos]
+        val data = level.getAttachedOrCreate(WitcheryFabric.LEVEL_DATA_TYPE).mutandisCacheMap[pos]
         if (data != null) {
-            level.getAttachedOrCreate(LEVEL_DATA_TYPE).mutandisCacheMap[pos] = MutandisData(data.tag, CACHE_LIFETIME)
+            level.getAttachedOrCreate(WitcheryFabric.LEVEL_DATA_TYPE).mutandisCacheMap[pos] = MutandisData(data.tag, CACHE_LIFETIME)
         }
     }
 
-    @JvmStatic
-    @Suppress("UnstableApiUsage")
-    val LEVEL_DATA_TYPE: AttachmentType<MutandisLevelAttachment> =
-        AttachmentRegistry.builder<MutandisLevelAttachment>()
-            .persistent(MutandisLevelDataAttachmentPlatform.CODEC)
-            .initializer { MutandisLevelAttachment() }
-            .buildAndRegister(MutandisLevelDataAttachmentPlatform.ID)
+
 }
