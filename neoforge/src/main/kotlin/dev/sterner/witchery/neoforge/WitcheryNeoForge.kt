1bfee8bbe942532b0d29cb6e09fb33eadebbb181
mrsterner <simon@sterner.dev>
2024-10-19 15:50:17 +0200

    fix neoforge

--- neoforge/src/main/kotlin/dev/sterner/witchery/neoforge/WitcheryNeoForge.kt
+++ neoforge/src/main/kotlin/dev/sterner/witchery/neoforge/WitcheryNeoForge.kt
@@ -4,10 +4,12 @@ import dev.architectury.registry.client.level.entity.EntityRendererRegistry
 import dev.sterner.witchery.Witchery
 import dev.sterner.witchery.client.model.AltarBlockEntityModel
 import dev.sterner.witchery.client.model.AltarClothBlockEntityModel
+import dev.sterner.witchery.client.model.JarModel
 import dev.sterner.witchery.client.particle.ColorBubbleParticle
 import dev.sterner.witchery.client.screen.AltarScreen
 import dev.sterner.witchery.client.screen.DistilleryScreen
 import dev.sterner.witchery.client.screen.OvenScreen
+import dev.sterner.witchery.neoforge.event.WitcheryNeoForgeEvents
 import dev.sterner.witchery.platform.neoforge.MutandisDataAttachmentImpl
 import dev.sterner.witchery.registry.*
 import net.minecraft.client.Minecraft
@@ -33,6 +35,7 @@ import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent
 import net.neoforged.neoforge.event.LootTableLoadEvent
 import net.neoforged.neoforge.registries.DataPackRegistryEvent
 import net.neoforged.neoforge.server.ServerLifecycleHooks
+import thedarkcolour.kotlinforforge.neoforge.forge.FORGE_BUS
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.neoforge.forge.runForDist
 
@@ -55,9 +58,12 @@ object WitcheryNeoForge {
             serverTarget = {
                 MOD_BUS.addListener(::onServerSetup)
                 MOD_BUS.addListener(::onLoadComplete)
+                FORGE_BUS.addListener(WitcheryNeoForgeEvents::modifyLootTable)
                 "test"
             }
         )
+
+
     }
 
     private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
@@ -85,44 +91,6 @@ object WitcheryNeoForge {
     }
 
     @SubscribeEvent
-    fun modifyLootTable(event: LootTableLoadEvent){
-        val registries: HolderLookup.Provider = if (ServerLifecycleHooks.getCurrentServer() != null) ServerLifecycleHooks.getCurrentServer()!!.registryAccess() else RegistryAccess.EMPTY
-
-        if (event.name.equals(EntityType.BAT.defaultLootTable)) {
-            val pool = LootPool.lootPool().add(
-                LootItem.lootTableItem(WitcheryItems.WOOL_OF_BAT.get())
-                    .`when`(LootItemRandomChanceCondition.randomChance(0.25f))
-                .apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0F, 1.0F)))
-                .apply(EnchantedCountIncreaseFunction.lootingMultiplier(registries, UniformGenerator.between(0.0F, 1.0F))))
-                .name("witchery_inject").build()
-
-            event.table.addPool(pool)
-        }
-
-        if (event.name.equals(EntityType.WOLF.defaultLootTable)) {
-            val pool = LootPool.lootPool().add(
-                LootItem.lootTableItem(WitcheryItems.TONGUE_OF_DOG.get())
-                    .`when`(LootItemRandomChanceCondition.randomChance(0.25f))
-                    .apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0F, 1.0F)))
-                    .apply(EnchantedCountIncreaseFunction.lootingMultiplier(registries, UniformGenerator.between(0.0F, 1.0F))))
-                .name("witchery_inject").build()
-
-            event.table.addPool(pool)
-        }
-
-        if (event.name.equals(EntityType.FROG.defaultLootTable)) {
-            val pool = LootPool.lootPool().add(
-                LootItem.lootTableItem(WitcheryItems.TOE_OF_FROG.get())
-                    .`when`(LootItemRandomChanceCondition.randomChance(0.25f))
-                    .apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0F, 1.0F)))
-                    .apply(EnchantedCountIncreaseFunction.lootingMultiplier(registries, UniformGenerator.between(0.0F, 1.0F))))
-                .name("witchery_inject").build()
-
-            event.table.addPool(pool)
-        }
-    }
-
-    @SubscribeEvent
     private fun registerLayerDefinitions(event: EntityRenderersEvent.RegisterLayerDefinitions) {
         event.registerLayerDefinition(
             AltarBlockEntityModel.LAYER_LOCATION,
@@ -130,6 +98,9 @@ object WitcheryNeoForge {
         event.registerLayerDefinition(
             AltarClothBlockEntityModel.LAYER_LOCATION,
             AltarClothBlockEntityModel::createBodyLayer)
+        event.registerLayerDefinition(
+            JarModel.LAYER_LOCATION,
+            JarModel::createBodyLayer)
     }
 
     @SubscribeEvent
