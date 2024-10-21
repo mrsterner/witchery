5b5e0613dfa5856ad824880d0abdb7110f597a8c
mrsterner <simon@sterner.dev>
2024-10-21 13:16:32 +0200

    babayaga hat effect

---
+++ common/src/main/kotlin/dev/sterner/witchery/handler/EquipmentHandler.kt
@@ -0,0 +1,75 @@
+package dev.sterner.witchery.handler
+
+import dev.architectury.event.EventResult
+import dev.sterner.witchery.platform.infusion.InfusionType
+import dev.sterner.witchery.platform.infusion.PlayerInfusionDataAttachment
+import dev.sterner.witchery.registry.WitcheryItems
+import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.sounds.SoundEvent
+import net.minecraft.sounds.SoundEvents
+import net.minecraft.sounds.SoundSource
+import net.minecraft.util.Mth
+import net.minecraft.world.damagesource.DamageSource
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.animal.Fox
+import net.minecraft.world.entity.player.Player
+
+
+object EquipmentHandler {
+
+    fun babaYagaHit(livingEntity: LivingEntity?, damageSource: DamageSource?, fl: Float): EventResult? {
+        if (livingEntity is Player && livingEntity.getItemBySlot(EquipmentSlot.HEAD).`is`(WitcheryItems.BABA_YAGAS_HAT.get())) {
+            if (PlayerInfusionDataAttachment.getPlayerInfusion(livingEntity).type != InfusionType.NONE) {
+                val level = livingEntity.level()
+
+                if (level.random.nextFloat() < 0.2f && fl > 0) {
+                    if (attemptTeleport(livingEntity, 20.0)) {
+
+                        level.playSound(
+                            null,
+                            livingEntity.getX(),
+                            livingEntity.getY(),
+                            livingEntity.getZ(),
+                            SoundEvents.CHORUS_FRUIT_TELEPORT,
+                            SoundSource.PLAYERS,
+                            0.5f, 1f
+                        )
+                        livingEntity.resetFallDistance()
+
+                        return EventResult.interruptFalse()
+                    }
+                }
+            }
+        }
+
+        return EventResult.pass()
+    }
+
+    private fun attemptTeleport(player: Player, distance: Double): Boolean {
+        val playerPos = player.onPos
+        val random = player.level().random
+
+        for (i in 0..15) {
+            val targetX = playerPos.x + Mth.nextDouble(random, -distance, distance)
+            val targetY = playerPos.y + Mth.nextDouble(random, -distance / 2f, distance / 2f)
+            val targetZ = playerPos.z + Mth.nextDouble(random, -distance, distance)
+
+            val mutable: BlockPos.MutableBlockPos = BlockPos.MutableBlockPos(targetX.toInt(), targetY.toInt(), targetZ.toInt())
+
+            while (mutable.y > 0 && !player.level().getBlockState(mutable).blocksMotion()) {
+                mutable.move(Direction.DOWN)
+            }
+
+            val blockBelow = player.level().getBlockState(mutable)
+            val blockAbove = player.level().getBlockState(mutable.above())
+
+            if (blockBelow.blocksMotion() && !blockAbove.blocksMotion()) {
+                player.teleportTo(mutable.x + 0.5, mutable.y + 1.0, mutable.z + 0.5)
+                return true
+            }
+        }
+        return false
+    }
+}
