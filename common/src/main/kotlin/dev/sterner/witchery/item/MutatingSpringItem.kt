e6d17d0ceee8ad8c7e66b497bba9ab2c705870f6
mrsterner <simon@sterner.dev>
2024-10-31 15:05:16 +0100

    wormwood maker

--- common/src/main/kotlin/dev/sterner/witchery/item/MutatingSpringItem.kt
+++ common/src/main/kotlin/dev/sterner/witchery/item/MutatingSpringItem.kt
@@ -1,9 +1,15 @@
 package dev.sterner.witchery.item
 
+import dev.sterner.witchery.registry.WitcheryBlocks
+import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.state.BlockState
 
 class MutatingSpringItem(properties: Properties) : Item(properties) {
 
@@ -11,6 +17,10 @@ class MutatingSpringItem(properties: Properties) : Item(properties) {
     override fun useOn(context: UseOnContext): InteractionResult {
         val level = context.level
         val pos = context.clickedPos
+
+        if (level.getBlockState(pos).`is`(Blocks.WHEAT)) {
+            makeWormwood(level, pos)
+        }
 
         val blockState = level.getBlockState(pos)
         if (blockState.`is`(Blocks.GRASS_BLOCK)) {
@@ -29,5 +39,41 @@ class MutatingSpringItem(properties: Properties) : Item(properties) {
 
 
         return super.useOn(context)
+    }
+
+    private fun makeWormwood(level: Level, pos: BlockPos) {
+        if (checkCardinal(level, pos, WitcheryBlocks.WISPY_COTTON.get()) && checkDiagonals(level, pos, Blocks.WATER)) {
+            level.setBlockAndUpdate(pos, WitcheryBlocks.WORMWOOD_CROP.get().defaultBlockState())
+            removeCardinal(level, pos)
+            removeDiagonals(level, pos)
+        }
+    }
+
+    private fun checkCardinal(level: Level, pos: BlockPos, block: Block): Boolean {
+        return level.getBlockState(pos.north()).`is`(block) &&
+                level.getBlockState(pos.south()).`is`(block) &&
+                level.getBlockState(pos.east()).`is`(block) &&
+                level.getBlockState(pos.west()).`is`(block)
+    }
+
+    private fun checkDiagonals(level: Level, pos: BlockPos, block: Block): Boolean {
+        return level.getBlockState(pos.north().east().below()).`is`(block) &&
+                level.getBlockState(pos.north().west().below()).`is`(block) &&
+                level.getBlockState(pos.south().east().below()).`is`(block) &&
+                level.getBlockState(pos.south().west().below()).`is`(block)
+    }
+
+    private fun removeCardinal(level: Level, pos: BlockPos) {
+        level.setBlockAndUpdate(pos.north(), Blocks.AIR.defaultBlockState())
+        level.setBlockAndUpdate(pos.south(), Blocks.AIR.defaultBlockState())
+        level.setBlockAndUpdate(pos.east(), Blocks.AIR.defaultBlockState())
+        level.setBlockAndUpdate(pos.west(), Blocks.AIR.defaultBlockState())
+    }
+
+    private fun removeDiagonals(level: Level, pos: BlockPos) {
+        level.setBlockAndUpdate(pos.north().east().below(), Blocks.AIR.defaultBlockState())
+        level.setBlockAndUpdate(pos.north().west().below(), Blocks.AIR.defaultBlockState())
+        level.setBlockAndUpdate(pos.south().east().below(), Blocks.AIR.defaultBlockState())
+        level.setBlockAndUpdate(pos.south().west().below(), Blocks.AIR.defaultBlockState())
     }
 }
