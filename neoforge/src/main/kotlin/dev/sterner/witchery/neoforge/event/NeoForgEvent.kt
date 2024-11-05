67ec5d15d9f26c465cf146824e8e97fd4054e1a5
mrsterner <simon@sterner.dev>
2024-11-05 10:23:48 +0100

    frikkin broom fix

--- neoforge/src/main/kotlin/dev/sterner/witchery/neoforge/event/NeoForgEvent.kt
+++ neoforge/src/main/kotlin/dev/sterner/witchery/neoforge/event/NeoForgEvent.kt
@@ -2,6 +2,7 @@ package dev.sterner.witchery.neoforge.event
 
 import dev.sterner.witchery.Witchery
 import dev.sterner.witchery.entity.BroomEntity
+import dev.sterner.witchery.registry.WitcheryKeyMappings
 import net.minecraft.world.entity.player.Player
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
@@ -12,10 +13,20 @@ object NeoForgEvent {
 
     @SubscribeEvent
     fun dismountBroom(event: EntityMountEvent) {
+        /*
+        if (!event.isMounting
+            && event.entityBeingMounted.isAlive
+            && event.entityMounting is Player
+            && event.entityBeingMounted is BroomEntity
+            && !WitcheryKeyMappings.BROOM_DISMOUNT_KEYMAPPING.isDown){
 
-        if (!event.level.isClientSide() && !event.isMounting && event.entityBeingMounted.isAlive && event.entityMounting is Player && event.entityBeingMounted is BroomEntity){
+            val player = event.entityMounting as Player
+            val broom = event.entityBeingMounted as BroomEntity
+            broom.inputShift = player.isShiftKeyDown
+
             event.isCanceled = true
         }
 
+         */
     }
 }
