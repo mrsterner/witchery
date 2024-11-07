1ec0bd888c750eb96460bf4559e805c9f04787bb
mrsterner <simon@sterner.dev>
2024-11-07 21:51:56 +0100

    spirit portal start part 5

--- common/src/main/kotlin/dev/sterner/witchery/block/spirit_portal/SpiritPortalBlock.kt
+++ common/src/main/kotlin/dev/sterner/witchery/block/spirit_portal/SpiritPortalBlock.kt
@@ -8,19 +8,23 @@ import dev.sterner.witchery.registry.WitcheryBlocks
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
-import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.BlockStateProperties.HORIZONTAL_FACING
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.world.phys.shapes.VoxelShape
 import java.util.function.Supplier
 
 class SpiritPortalBlock(properties: Properties) : WitcheryBaseEntityBlock(properties) {
 
     init {
         this.registerDefaultState(
-            stateDefinition.any().setValue(BlockStateProperties.HORIZONTAL_FACING, Direction.NORTH)
+            stateDefinition.any().setValue(HORIZONTAL_FACING, Direction.NORTH)
         )
     }
 
@@ -30,21 +34,42 @@ class SpiritPortalBlock(properties: Properties) : WitcheryBaseEntityBlock(proper
 
     override fun getStateForPlacement(blockPlaceContext: BlockPlaceContext): BlockState {
         return defaultBlockState().setValue(
-            BlockStateProperties.HORIZONTAL_FACING,
+            HORIZONTAL_FACING,
             blockPlaceContext.horizontalDirection.opposite
         )
     }
 
     override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
-        builder.add(BlockStateProperties.HORIZONTAL_FACING)
+        builder.add(HORIZONTAL_FACING)
     }
 
+    override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
+        val dir = state.getValue(HORIZONTAL_FACING)
+        return when (dir) {
+            Direction.NORTH -> {
+                NORTH
+            }
+            Direction.EAST -> {
+                EAST
+            }
+            Direction.WEST -> {
+                WEST
+            }
+            else -> {
+                SOUTH
+            }
+        }
+    }
 
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity? {
         return WitcheryBlockEntityTypes.SPIRIT_PORTAL.get().create(pos, state)
     }
 
     companion object {
+        val SOUTH: VoxelShape = Shapes.create(0.0, 0.0, 13.0 / 16, 1.0, 1.0, 1.0)
+        val EAST: VoxelShape = Shapes.create(13.0 / 16, 0.0, 0.0, 1.0, 1.0, 1.0)
+        val WEST: VoxelShape = Shapes.create(0.0, 0.0, 0.0, 3.0 / 16, 1.0, 1.0)
+        val NORTH: VoxelShape = Shapes.create(0.0, 0.0, 0.0, 1.0, 1.0, 3.0 / 16)
 
         val STRUCTURE: Supplier<MultiBlockHorizontalDirectionStructure> =
             Supplier<MultiBlockHorizontalDirectionStructure> {
@@ -69,6 +94,5 @@ class SpiritPortalBlock(properties: Properties) : WitcheryBaseEntityBlock(proper
                     ),
                 ))
             }
-
     }
 }
--- common/src/main/kotlin/dev/sterner/witchery/block/spirit_portal/SpiritPortalBlockComponent.kt
+++ common/src/main/kotlin/dev/sterner/witchery/block/spirit_portal/SpiritPortalBlockComponent.kt
@@ -1,31 +1,57 @@
 package dev.sterner.witchery.block.spirit_portal
 
 import dev.sterner.witchery.api.multiblock.MultiBlockComponentBlock
+import dev.sterner.witchery.block.spirit_portal.SpiritPortalBlock.Companion.EAST
+import dev.sterner.witchery.block.spirit_portal.SpiritPortalBlock.Companion.NORTH
+import dev.sterner.witchery.block.spirit_portal.SpiritPortalBlock.Companion.SOUTH
+import dev.sterner.witchery.block.spirit_portal.SpiritPortalBlock.Companion.WEST
+import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
-import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.BlockStateProperties.HORIZONTAL_FACING
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.VoxelShape
 
 class SpiritPortalBlockComponent(properties: Properties) : MultiBlockComponentBlock(properties.noOcclusion()) {
 
     init {
         this.registerDefaultState(
-            stateDefinition.any().setValue(BlockStateProperties.HORIZONTAL_FACING, Direction.NORTH)
+            stateDefinition.any().setValue(HORIZONTAL_FACING, Direction.NORTH)
         )
     }
 
     override fun getStateForPlacement(blockPlaceContext: BlockPlaceContext): BlockState {
         return defaultBlockState().setValue(
-            BlockStateProperties.HORIZONTAL_FACING,
+            HORIZONTAL_FACING,
             blockPlaceContext.horizontalDirection.opposite
         )
     }
 
+    override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
+        val dir = state.getValue(HORIZONTAL_FACING)
+        return when (dir) {
+            Direction.NORTH -> {
+                NORTH
+            }
+            Direction.EAST -> {
+                EAST
+            }
+            Direction.WEST -> {
+                WEST
+            }
+            else -> {
+                SOUTH
+            }
+        }
+    }
+
     override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
-        builder.add(BlockStateProperties.HORIZONTAL_FACING)
+        builder.add(HORIZONTAL_FACING)
     }
 
     override fun getRenderShape(state: BlockState): RenderShape {
