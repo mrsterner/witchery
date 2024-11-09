511933cb9f19fbcbc52044a64c557feea10b6405
mrsterner <simon@sterner.dev>
2024-11-09 15:50:58 +0100

    added brew of ink tweak

--- common/src/main/kotlin/dev/sterner/witchery/item/brew/BrewOfInk.kt
+++ common/src/main/kotlin/dev/sterner/witchery/item/brew/BrewOfInk.kt
@@ -4,6 +4,7 @@ import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.effect.MobEffects
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.animal.Animal
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.Level
 import net.minecraft.world.phys.HitResult
 
@@ -11,5 +12,8 @@ class BrewOfInk(color: Int, properties: Properties) : ThrowableBrewItem(color, p
 
     override fun applyEffect(level: Level, livingEntity: LivingEntity?, result: HitResult) {
         livingEntity?.addEffect(MobEffectInstance(MobEffects.BLINDNESS, 20 * 8, 1))
+        if (livingEntity !is Player) {
+            livingEntity?.addEffect(MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20 * 8, 2))
+        }
     }
 }
