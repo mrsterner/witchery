299ee9766b980125f45a4ea074917e50a5f4c2fc
mrsterner <simon@sterner.dev>
2024-11-10 14:31:27 +0100

    api

--- common/src/main/kotlin/dev/sterner/witchery/api/WitcheryApi.kt
+++ common/src/main/kotlin/dev/sterner/witchery/api/WitcheryApi.kt
@@ -12,17 +12,25 @@ object WitcheryApi {
         return dim == WitcheryWorldgenKeys.NIGHTMARE || dim == WitcheryWorldgenKeys.DREAM
     }
 
+
     fun isInSpiritWorld(level: Level): Boolean {
         val dim = level.dimension()
         return dim == WitcheryWorldgenKeys.NIGHTMARE || dim == WitcheryWorldgenKeys.DREAM
     }
 
+    /**
+     * Used to make the player a witch, Witchery will be stronger towards this player
+     */
     fun makePlayerWitchy(player: Player) {
         if (!PlayerMiscDataAttachment.getData(player).isWitcheryAligned) {
             PlayerMiscDataAttachment.setWitcheryAligned(player, true)
         }
     }
 
+    /**
+     * True if the player is considered a witch. Witchery against this player will be stronger.
+     * This to not wreck players who are not interested in witchery
+     */
     fun isWitchy(maybeEntity: Player): Boolean {
         return PlayerMiscDataAttachment.getData(maybeEntity).isWitcheryAligned
     }
