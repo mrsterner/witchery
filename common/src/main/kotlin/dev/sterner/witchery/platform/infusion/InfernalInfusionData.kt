c640f594f4c874931477718766f7878afbbfc72e
mrsterner <simon@sterner.dev>
2024-11-10 23:32:13 +0100

    infernal: skeleton active

--- common/src/main/kotlin/dev/sterner/witchery/platform/infusion/InfernalInfusionData.kt
+++ common/src/main/kotlin/dev/sterner/witchery/platform/infusion/InfernalInfusionData.kt
@@ -4,8 +4,8 @@ import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import dev.sterner.witchery.Witchery
 import dev.sterner.witchery.platform.infusion.InfusionData.Companion.MAX_CHARGE
-import dev.sterner.witchery.registry.WitcheryEntityTypes
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.sounds.SoundEvents
 import net.minecraft.util.StringRepresentable
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.effect.MobEffects
@@ -13,9 +13,14 @@ import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LightningBolt
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.entity.projectile.AbstractArrow
+import net.minecraft.world.entity.projectile.ProjectileUtil
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
 import net.minecraft.world.level.Level
 import net.minecraft.world.phys.HitResult
 import net.minecraft.world.phys.Vec3
+import kotlin.math.sqrt
 
 class InfernalInfusionData(val currentCreature: CreatureType = CreatureType.NONE) {
 
@@ -63,128 +68,138 @@ class InfernalInfusionData(val currentCreature: CreatureType = CreatureType.NONE
 
     enum class CreatureType(val entityType: EntityType<*>?): StringRepresentable {
         NONE(null) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         CREEPER(EntityType.CREEPER) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         SKELETON(EntityType.SKELETON) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+                val itemStack: ItemStack = Items.BOW.defaultInstance
+                val itemStack2: ItemStack = Items.ARROW.defaultInstance
+                val abstractArrow: AbstractArrow = ProjectileUtil.getMobArrow(player, itemStack2, 1.0f, itemStack)
+                val d: Double = lookVec.x() - player.x
+                val e: Double = lookVec.y() - abstractArrow.y + 0.3333
+                val f: Double = lookVec.z() - player.z
+                val g = sqrt(d * d + f * f)
+                abstractArrow.shoot(d, e + g * 0.2f, f, 1.6f, (14 - level.difficulty.id * 4).toFloat())
+                player.playSound(SoundEvents.SKELETON_SHOOT, 1.0f, 1.0f / (level.getRandom().nextFloat() * 0.4f + 0.8f))
+                level.addFreshEntity(abstractArrow)
                 return false
             }
         },
-        SPIDER(EntityType.SKELETON) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+        SPIDER(EntityType.SPIDER) {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         ZOMBIE(EntityType.ZOMBIE) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         SLIME(EntityType.SLIME) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         GHAST(EntityType.GHAST) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         ZOMBIE_PIGMAN(EntityType.ZOMBIFIED_PIGLIN) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         ENDERMAN(EntityType.ENDERMAN) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         CAVE_SPIDER(EntityType.CAVE_SPIDER) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         SILVERFISH(EntityType.SILVERFISH) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         BLAZE(EntityType.BLAZE) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         MAGMA_CUBE(EntityType.MAGMA_CUBE) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         BAT(EntityType.BAT) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         PIG(EntityType.PIG) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         SHEEP(EntityType.SHEEP) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         COW(EntityType.COW) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         VILLAGER(EntityType.VILLAGER) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         CHICKEN(EntityType.CHICKEN) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         SQUID(EntityType.SQUID) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         WOLF(EntityType.WOLF) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         MOOSHROOM(EntityType.MOOSHROOM) {
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         OCELOT(EntityType.OCELOT) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         },
         HORSE(EntityType.HORSE) { //PASSIVE DONE
-            override fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
+            override fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean {
                 return false
             }
         };
 
 
-        abstract fun usePower(level: Level, lookVec: Vec3, hitResult: HitResult): Boolean
+        abstract fun usePower(player: Player, level: Level, lookVec: Vec3, hitResult: HitResult): Boolean
 
         fun getCost() : Int {
             return 100
