599d50e61e7540662ef96a20acd64db39d7b2d7c
mrsterner <simon@sterner.dev>
2024-11-12 22:32:08 +0100

    3 curses

--- common/src/main/kotlin/dev/sterner/witchery/curse/CurseOfMisfortune.kt
+++ common/src/main/kotlin/dev/sterner/witchery/curse/CurseOfMisfortune.kt
@@ -1,6 +1,36 @@
 package dev.sterner.witchery.curse
 
 import dev.sterner.witchery.api.Curse
+import net.minecraft.world.effect.MobEffectInstance
+import net.minecraft.world.effect.MobEffects
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
 
 class CurseOfMisfortune : Curse() {
+
+    override fun onTickCurse(level: Level, player: Player) {
+
+        if (level.gameTime % 20 == 0L) {
+            if (level.random.nextDouble() < 0.01) {
+                player.addEffect(MobEffectInstance(MobEffects.DIG_SLOWDOWN, 20 * 2, 0))
+            }
+            if (level.random.nextDouble() < 0.01) {
+                player.addEffect(MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20 * 2, 0))
+            }
+            if (level.random.nextDouble() < 0.01) {
+                player.addEffect(MobEffectInstance(MobEffects.BLINDNESS, 20 * 2, 0))
+            }
+            if (level.random.nextDouble() < 0.01) {
+                player.addEffect(MobEffectInstance(MobEffects.WEAKNESS, 20 * 2, 0))
+            }
+            if (level.random.nextDouble() < 0.01) {
+                player.addEffect(MobEffectInstance(MobEffects.GLOWING, 20 * 2, 0))
+            }
+            if (level.random.nextDouble() < 0.01) {
+                player.addEffect(MobEffectInstance(MobEffects.DARKNESS, 20 * 2, 0))
+            }
+        }
+
+        super.onTickCurse(level, player)
+    }
 }
--- common/src/main/kotlin/dev/sterner/witchery/curse/CurseOfOverheating.kt
+++ common/src/main/kotlin/dev/sterner/witchery/curse/CurseOfOverheating.kt
@@ -1,6 +1,22 @@
 package dev.sterner.witchery.curse
 
 import dev.sterner.witchery.api.Curse
+import dev.sterner.witchery.registry.WitcheryTags
+import net.minecraft.tags.BiomeTags
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
 
 class CurseOfOverheating : Curse() {
+
+    override fun onTickCurse(level: Level, player: Player) {
+
+        val biome = level.getBiome(player.blockPosition())
+
+        if (biome.`is`(BiomeTags.IS_SAVANNA) || biome.`is`(BiomeTags.IS_NETHER) || biome.`is`(BiomeTags.HAS_DESERT_PYRAMID)) {
+            player.remainingFireTicks = 20
+        }
+
+
+        super.onTickCurse(level, player)
+    }
 }
