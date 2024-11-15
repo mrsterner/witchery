255031c2dd7d105b906cd9ce10967ad823f56010
mrsterner <simon@sterner.dev>
2024-11-15 17:09:35 +0100

    graveyard stuff

---
+++ common/src/main/java/dev/sterner/witchery/mixin/BrushItemMixin.java
@@ -0,0 +1,8 @@
+package dev.sterner.witchery.mixin;
+
+import net.minecraft.world.item.BrushItem;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(BrushItem.class)
+public class BrushItemMixin {
+}
