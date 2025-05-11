c06a35ced3d3286dc9c8c6e71dd81e0c3a0344bc
mrsterner <simon@sterner.dev>
2025-05-11 20:35:48 +0200

    baba yaga stuff

--- common/src/main/kotlin/dev/sterner/witchery/block/coffin/CoffinBlock.kt
+++ common/src/main/kotlin/dev/sterner/witchery/block/coffin/CoffinBlock.kt
@@ -3,9 +3,7 @@ package dev.sterner.witchery.block.coffin
 import dev.sterner.witchery.api.block.WitcheryBaseBlockEntity
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
-import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
-import net.minecraft.world.ItemInteractionResult
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.DyeColor
